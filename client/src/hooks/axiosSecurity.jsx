d4d2d03beb009422d9817d3d7087b83d25b7dbe1
shubhamrathod0015 <shubhamrathod664@gmail.com>
2026-01-15 17:48:57 +0530

    feat: introduce custom Axios instances for API communication, including a secured one with authentication and authorization interceptors.

--- client/src/hooks/axiosSecurity.jsx
+++ client/src/hooks/axiosSecurity.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import useAuth from "./useAuth";
 
 const axiosSecurity = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxiosSecurity = () => {
